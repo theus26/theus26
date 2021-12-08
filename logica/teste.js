d690adffe4c2691786882b8f351ebc50955b86e9
Matheus <theuslipe26@gmail.com>
2021-12-08 13:21:29 -0300

    confirmar

---
+++ logica/teste.js
@@ -0,0 +1,7 @@
+var numero1 = 1;
+var numero2 = 2;
+var numero3 = 3;
+
+console.log('primeiro numero: '+ numero1);
+console.log('segundo numero: '+ numero2);
+console.log('terceiro numero: '+ numero3);
