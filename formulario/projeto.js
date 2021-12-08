85b9bda6f59ccadd727543e1dac32b3771ab250a
Matheus <theuslipe26@gmail.com>
2021-12-08 10:01:06 -0300

    repositorio html+css+js

---
+++ formulario/projeto.js
@@ -0,0 +1,12 @@
+ function eventoClick(){
+    var decisao = prompt("Você prencheu o formulario?")
+    if (decisao.toLowerCase()=== "sim"){
+        alert("Informações Enviadas !");
+    }
+   
+ } 
+    
+ 
+    
+     
+    
