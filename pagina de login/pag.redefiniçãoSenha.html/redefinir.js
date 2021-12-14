44cac38efe026520d1b329b1fec1d1ace614a95d
Matheus <theuslipe26@gmail.com>
2021-12-14 20:02:00 -0300

    confirmar

---
+++ pagina de login/pag.redefiniçãoSenha.html/redefinir.js
@@ -0,0 +1,9 @@
+function redefinir(){
+   if (document.getElementById('email').value.length<3) {
+       alert("Preencha o campo com seu email");
+   } else {
+       alert("Email enviado para redefinição de senha ");
+   }
+   
+}
+// esta fazendo a verificação no input, se dentro do input tiver um valor menor que 3 ele diz algo
