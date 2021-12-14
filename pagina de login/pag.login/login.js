3b2cfc85d329f5cc1700da7018d72fa35e7e4421
Matheus <theuslipe26@gmail.com>
2021-12-14 11:56:10 -0300

    confirmar

--- pagina de login/pag.login/login.js
+++ pagina de login/pag.login/login.js
@@ -1,3 +1,11 @@
-function(){
-    
+function acessar(){
+    var nome = (document.getElementById("nome").value);
+    var senha = (document.getElementById("senha").value)
+    if (nome === "matheusadivir@hotmail.com" && senha === "adivir88") {
+        alert("entrou!");
+    } else{
+        alert("usuario e senha inexistente!");
+    }
 }
+
+  
