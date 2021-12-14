9b615d4299b7cc80c57212274910bb1890d4c76a
Matheus <theuslipe26@gmail.com>
2021-12-14 11:56:38 -0300

    confirmar

--- logica/teste.js
+++
@@ -1,7 +0,0 @@
-var numero1 = 1;
-var numero2 = 2;
-var numero3 = 3;
-
-console.log('primeiro numero: '+ numero1);
-console.log('segundo numero: '+ numero2);
-console.log('terceiro numero: '+ numero3);
--- logica/teste2.js
+++
@@ -1,1 +0,0 @@
-var num = console.log('digite um numero ');
