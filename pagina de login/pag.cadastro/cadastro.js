0acaa0ca4354584a43fbf3280a89193d08be6c23
Matheus <theuslipe26@gmail.com>
2021-12-15 10:33:51 -0300

    confirmar

---
+++ pagina de login/pag.cadastro/cadastro.js
@@ -0,0 +1,10 @@
+function cadastro(){
+    if(document.getElementById ('nome' && 'email' && 'senha') .value.length<3){
+        alert("preencha formulario !")
+    }else{
+        alert("Conta criada com sucesso !")
+    }
+   
+    
+ }
+ // esta fazendo a verificação no input, se dentro do input tiver um valor menor que 3 ele diz algo
